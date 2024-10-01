f556020d485183c6fa608571824696daef23b412
kazu0 <kazuooficial@hotmail.com>
2024-09-30 23:11:45 -0300

    CUBE

--- PSYCHOTIC/res/shaders/shader.vert
+++ PSYCHOTIC/res/shaders/shader.vert
@@ -1,20 +1,45 @@
 #version 460 core
 
+layout (std140, binding = 0) uniform per_frame_data {
+	uniform mat4 mvp;
+	uniform int is_wire_frame;
+};
+
 layout (location = 0) out vec3 out_color;
 
-const vec2 pos[3] = vec2[3](
-	vec2(-0.5, -0.5),
-	vec2( 0.5, -0.5),
-	vec2( 0.0,  0.5)
+const vec3 pos[8] = vec3[8](
+	vec3(-1.0, -1.0, 1.0), vec3( 1.0, -1.0, 1.0),
+	vec3( 1.0,  1.0, 1.0), vec3(-1.0,  1.0, 1.0),
+
+	vec3(-1.0, -1.0, -1.0), vec3( 1.0, -1.0, -1.0),
+	vec3( 1.0,  1.0, -1.0), vec3(-1.0,  1.0, -1.0)
 );
 
-const vec3 col[3] = vec3[3](
-	vec3(1.0, 0.0, 0.0),
-	vec3(0.0, 1.0, 0.0),
-	vec3(0.0, 0.0, 1.0)
+const vec3 col[8] = vec3[8](
+	vec3(1.0, 0.0, 0.0), vec3(1.0, 1.0, 0.0),
+	vec3(0.0, 0.0, 1.0), vec3(1.0, 1.0, 0.0),
+
+	vec3(1.0, 1.0, 0.0), vec3(0.0, 0.0, 1.0),
+	vec3(0.0, 1.0, 0.0), vec3(1.0, 0.0, 0.0)
+);
+
+const int indices[36] = int[36](
+	// front
+	0, 1, 2, 2, 3, 0,
+	// right
+	1, 5, 6, 6, 2, 1,
+	// back
+	7, 6, 5, 5, 4, 7,
+	// left
+	4, 0, 3, 3, 7, 4,
+	// bottom
+	4, 5, 1, 1, 0, 4,
+	// top
+	3, 2, 6, 6, 7, 3
 );
 
 void main() {
-	gl_Position = vec4(pos[gl_VertexID], 0.0, 1.0);
-	out_color = col[gl_VertexID];
+	int i = indices[gl_VertexID];
+	gl_Position = mvp * vec4(pos[i], 1.0);
+	out_color = is_wire_frame > 0 ? vec3(0.0) : col[i];
 }
--- PSYCHOTIC/src/main.cpp
+++ PSYCHOTIC/src/main.cpp
@@ -1,9 +1,17 @@
+#include <glad/glad.h>
+#include <glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <fstream>
 #include <string>
 
-#include <glad/glad.h>
-#include <glfw3.h>
+struct per_frame_data {
+	glm::mat4 mvp;
+	int is_wire_frame;
+};
 
 struct window_desc {
 	const char *title;
@@ -19,9 +27,9 @@ static window_state window;
 
 void glfw_error_callback(int error, const char* description);
 void glfw_key_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
-void glfw_window_size_callback(GLFWwindow *window, int width, int height);
 
 std::string read_text_from_file(const char *path);
+int check_shader(unsigned int shader, const char *type);
 
 int main() {
 	window_desc desc = {};
@@ -55,10 +63,7 @@ int main() {
 	}
 
 	glfwSetWindowPos(window.handle, (mode->width - desc.width) / 2, (mode->height - desc.height) / 2);
-	
 	glfwSetKeyCallback(window.handle, glfw_key_callback);
-	glfwSetWindowSizeCallback(window.handle, glfw_window_size_callback);
-
 	glfwMakeContextCurrent(window.handle);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
@@ -71,10 +76,6 @@ int main() {
 
 	//
 	//
-	GLuint vao;
-	glCreateVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-
 	std::string vertex_source = read_text_from_file("res/shaders/shader.vert");
 	std::string fragment_source = read_text_from_file("res/shaders/shader.frag");
 
@@ -84,28 +85,69 @@ int main() {
 	const GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertex_shader, 1, &vertex_string, nullptr);
 	glCompileShader(vertex_shader);
+	check_shader(vertex_shader, "VERTEX SHADER");
 
 	const GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragment_shader, 1, &fragment_string, nullptr);
 	glCompileShader(fragment_shader);
+	check_shader(fragment_shader, "FRAGMENT SHADER");
 
 	const GLuint program = glCreateProgram();
 	glAttachShader(program, vertex_shader);
 	glAttachShader(program, fragment_shader);
 	glLinkProgram(program);
+	check_shader(program, "PROGRAM");
+	glUseProgram(program);
 
+	GLuint vao;
+	glCreateVertexArrays(1, &vao);
+	glBindVertexArray(vao);
 	
-	while (!glfwWindowShouldClose(window.handle)) {
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+	//
+	//
+	const GLsizeiptr buffer_size = sizeof(per_frame_data);
 
-		glUseProgram(program);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+	GLuint per_frame_data_buffer;
+	glCreateBuffers(1, &per_frame_data_buffer);
+	glNamedBufferStorage(per_frame_data_buffer, buffer_size, nullptr, GL_DYNAMIC_STORAGE_BIT);
+	glBindBufferRange(GL_UNIFORM_BUFFER, 0, per_frame_data_buffer, 0, buffer_size);
+
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_POLYGON_OFFSET_LINE);
+	glPolygonOffset(-1.0f, -1.0f);
+
+	while (!glfwWindowShouldClose(window.handle)) {
+		int width, height;
+		glfwGetFramebufferSize(window.handle, &width, &height);
+		const float ratio = width / (float)height;
+
+		glViewport(0, 0, width, height);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		const glm::mat4 model = glm::rotate(
+			glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.5f)), 
+			(float)glfwGetTime(), 
+			glm::vec3(1.0f, 1.0f, 1.0f));
+		const glm::mat4 projection = glm::perspective(45.0f, ratio, 0.1f, 10.0f);
+
+		per_frame_data frame_data = {};
+		frame_data.mvp = projection * model;
+		frame_data.is_wire_frame = false;
+		glNamedBufferSubData(per_frame_data_buffer, 0, buffer_size, &frame_data);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		frame_data.is_wire_frame = true;
+		glNamedBufferSubData(per_frame_data_buffer, 0, buffer_size, &frame_data);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		glfwSwapBuffers(window.handle);
 		glfwPollEvents();
 	}
 
+	glDeleteBuffers(1, &per_frame_data_buffer);
 	glDeleteProgram(program);
 	glDeleteShader(fragment_shader);
 	glDeleteShader(vertex_shader);
@@ -137,6 +179,22 @@ std::string read_text_from_file(const char *path) {
 	return str;
 }
 
-void glfw_window_size_callback(GLFWwindow *window, int width, int height) {
-	glViewport(0, 0, width, height);
+int check_shader(unsigned int shader, const char *type) {
+	int success;
+	char infoLog[1024];
+	if (type != "PROGRAM") {
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+		if (!success) {
+			glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+			printf("Shader compilation error type: %s\n%s", type, infoLog);
+		}
+	} else {
+		glGetProgramiv(shader, GL_LINK_STATUS, &success);
+		if (!success) {
+			glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+			printf("Program linkning error type: %s\n%s", type, infoLog);
+		}
+	}
+	return success;
 }
+
